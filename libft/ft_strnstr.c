5304107447a22521bafc86a453eaa7d3c9f1b3ca
jsilance <juliensilance@gmail.com>
2021-07-16 22:49:09 +0200

    repairs and '<' implementation

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jsilance <jsilance@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/06 14:06:14 by chly-huc          #+#    #+#             */
-/*   Updated: 2021/07/15 16:13:37 by jsilance         ###   ########.fr       */
+/*   Updated: 2021/07/16 22:49:04 by jsilance         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (!needle[0])
 		return ((char *)haystack);
 	j = 0;
-	while (haystack[++i] && i < len && !j)
+	while (haystack[++i] && i < len)
 		while (haystack[i + j] == needle[j] && i + j < len)
 			if (!needle[++j])
 				return (&((char *)haystack)[i]);

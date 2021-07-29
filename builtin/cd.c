20367f634101d1b4405c635eaf7cc5cef1fed5df
Charli Ly huchard <chly-huc@fu-r6-p1.s19.be>
2021-07-29 19:16:25 +0200

    cd?

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,21 +6,48 @@
 /*   By: chly-huc <chly-huc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 17:47:03 by chly-huc          #+#    #+#             */
-/*   Updated: 2021/07/25 00:10:42 by chly-huc         ###   ########.fr       */
+/*   Updated: 2021/07/29 19:15:41 by chly-huc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../struct/struct.h"
 
+void engine_cd(t_sh *sh, int *ret, char *str)
+{
+	env_lstedit(sh, "OLDPWD", get_actual_path());
+	*ret = chdir(env_lstcontent(sh, str));
+	env_lstedit(sh, "PWD", get_actual_path());
+}
+
 void ft_cd(t_sh *sh)
 {
-	int ret;
+	errno = 0;
+	char *tmp;
+	
 	sh->ptr_cmd = sh->lst_cmd;
 	sh->ptr_env = sh->lst_env;
-	ret = 0;
 	
+	if (!env_lstfinder(sh->ptr_env, "PWD"))
+		tmp = NULL;
+	else
+		tmp = ft_strdup(env_lstfinder(sh->ptr_env, "PWD")->content);
+	if (!sh->ptr_cmd->next || !ft_strcmp(((t_lst_cmd*)sh->ptr_cmd->next)->cmd,"~"))
+	{
+		engine_cd(sh, &errno, "HOME");
+		return;
+	}
+	sh->ptr_cmd = sh->ptr_cmd->next;
 	if (sh->ptr_cmd->next)
-		sh->ptr_cmd = sh->ptr_cmd->next;
-	if (!ft_strcmp(sh->ptr_cmd->cmd, "~"))
-		ret = chdir(env_lstfinder(sh->ptr_env, "HOME")->content);
+	{
+		ft_putstr_fd("My Minishell: cd:", 2);
+		ft_putstr_fd(" too many arguments\n", 2);
+		return ;
+	}
+	errno = chdir(sh->ptr_cmd->cmd);
+	if (errno == -1)
+	{
+		ft_putstr_fd("My Minishell: cd: ", 2);
+		ft_putstr_fd(sh->ptr_cmd->cmd, 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+	}
 }
--- error/error.c
+++ error/error.c
@@ -6,7 +6,7 @@
 /*   By: chly-huc <chly-huc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:01:52 by chly-huc          #+#    #+#             */
-/*   Updated: 2021/07/29 18:29:33 by chly-huc         ###   ########.fr       */
+/*   Updated: 2021/07/29 19:13:19 by chly-huc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int error(char *str, int error)
 	}
 	if (errno == PERM_DENIED)
 	{
+		
 		ft_putstr_fd("My Minishell: ", 2);
 		ft_putstr_fd(str, 2);
 		ft_putstr_fd(": Permission denied\n", 2);
